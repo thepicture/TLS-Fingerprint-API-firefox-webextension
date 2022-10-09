597f16e11171b693abec06d1dec12badea0c6fe3
thepicture <85333076+thepicture@users.noreply.github.com>
2022-10-09 19:51:44 +0500

    refactor: move proxy settings to module

--- background.js
+++ background.js
@@ -1,16 +1,27 @@
-function passRequestThroughTLSFingerprintAPI({ url }) {
-    if (url.startsWith('http://127.0.0.1:8082'))
-        return;
-    
-    const username = 'USERNAME'
-    const password = 'PASSWORD'
-    const host = 'HOST'
-    const port = -1;
-    const proxy = `http://${username}:${password}@${host}:${port}`
+import { proxy } from './proxy.js';
+const _proxy = proxy;
 
-    return {
-        redirectUrl: 'http://127.0.0.1:8082?poptls-url=' + encodeURIComponent(url) + '&poptls-proxy=' + encodeURIComponent(proxy)
-    };
+const passRequestThroughTLSFingerprintAPI = ({ url }) => {
+    try {
+        if (url.startsWith('http://127.0.0.1:8082'))
+        return;
+        const username = _proxy.user();
+        const password = _proxy.password();
+        const host = _proxy.host;
+        let port;
+        if (typeof _proxy.port === 'object') {
+            port = Math.floor(Math.random() * (_proxy.port.max - _proxy.port.min + 1) + _proxy.port.min);
+        } else {
+            port = _proxy.port;
+        }
+        const proxy = `http://${username}:${password}@${host}:${port}`;
+        
+        return {
+            redirectUrl: 'http://127.0.0.1:8082?poptls-url=' + encodeURIComponent(url) + '&poptls-proxy=' + encodeURIComponent(proxy)
+        };
+    } catch (error) {
+        console.error('error', error);
+    }
 }
 
 browser.webRequest.onBeforeSendHeaders.addListener(
